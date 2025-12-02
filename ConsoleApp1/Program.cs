99ff75fdbba5b806586ad0efde1cf56a3ee4200b
Sümeyye Çekiç <sumicekic@gmail.com>
2025-12-02 16:55:25 +0300

    proje ilk adım, holiday sınıfı oluşturuldu.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// kütüphane entegre ediyoruz
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json; 
+
+namespace TatilSistemi
+{
+    class Program
+    {
+        // veirleri saklama
+        static List<Holiday> tumTatiller = new List<Holiday>();
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Tatil Takip Sistemi Hazırlanıyor...");
+            //kod gelecek
+            Console.ReadLine();
+        }
+    }
+    public class Holiday
+    {
+        public string date { get; set; }        
+        public string localName { get; set; }   
+        public string name { get; set; }         
+        public string countryCode { get; set; } 
+        public bool @fixed { get; set; }        
+        public bool global { get; set; }        
+    }
+}
